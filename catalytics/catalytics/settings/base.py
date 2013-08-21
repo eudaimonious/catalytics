f21ddfc18935ff8edfff830558e773049a1aeee3
Margaret Scott <eudaimonious@gmail.com>
2013-08-21 15:01:58 -0400

    Enable Django Admin

--- catalytics/catalytics/settings/base.py
+++ catalytics/catalytics/settings/base.py
@@ -67,7 +67,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+# 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -77,7 +77,7 @@ SECRET_KEY = ')#b=&-imp^ov6!zg@5(-s8x$xjti&w&szm!y=-s$^og&0b9yr2'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+# 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -109,9 +109,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'food',
+    'grocerylists'
 )
 
 # A sample logging configuration. The only tangible logging
